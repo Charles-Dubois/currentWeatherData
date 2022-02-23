61c83b0f82aca1e1b8804b017d082adac983ecd4
Charles Dubois <charles.dubois.h@gmail.com>
2022-02-23 11:04:17 +0100

    initalisation d'un state, d'un contexte, création du footer et de la navbar en utlisant un router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import Home from "./views/Home";
+import Favorites from "./views/Favorites";
+import { createContext, useState } from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+export const NameContext = createContext("light");
+export default function App() {
+  const [setUpState, setSetUpState] = useState("");
+  const context = {
+    setUpState: setUpState,
+    setSetUpState: setSetUpState,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NameContext.Provider value={context}>
+        <BrowserRouter>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/favorites">favorites</Link>
+          </nav>
+
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/favorites" component={Favorites}></Route>
+          </Switch>
+          <footer>
+            <h3>Charles Dubois</h3>
+            <p>{new Date().toLocaleDateString("fr")}</p>
+          </footer>
+        </BrowserRouter>
+      </NameContext.Provider>
+    </>
   );
 }
-
-export default App;
